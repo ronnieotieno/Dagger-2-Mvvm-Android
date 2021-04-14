bbef9a3fcc9d42ef10a6ee45837fc19bdc90924d
ronnieotieno <ronnieotieno2@gmail.com>
2021-04-14 15:39:19 +0300

    Fix: Added a new Unsplash acces key

--- app/src/main/java/dev/ronnie/imageloaderdagger2/utils/Config.kt
+++ app/src/main/java/dev/ronnie/imageloaderdagger2/utils/Config.kt
@@ -1,6 +1,6 @@
 package dev.ronnie.imageloaderdagger2.utils
 
 object Config {
-    const val unsplash_access_key = "k9xKYzhpvSyzo18v3uM7eN220qB2J9Im81JhQdpZv2o"
+    const val unsplash_access_key = "NkKPE2h5OQzSHJd0_SiXCfgddN4YgKbC6dVWfPk7Elg"
     const val BASE_URL_UNSPLASH = "https://api.unsplash.com/"
 }
